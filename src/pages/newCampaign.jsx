563b8115e3c992487b17e5b1b85e6b8ca0e84083
Fawole Ajibola Lukman <jibolafawole258@gmail.com>
2024-06-20 10:15:44 +0100

    quick fix

--- src/pages/newCampaign.jsx
+++ src/pages/newCampaign.jsx
@@ -14,17 +14,17 @@ const Bottom = styled.div``;
 const NewCampaign = () => {
   const [checked, setchecked] = useState(true);
   const [modalOpen, setModalOpen] = useState(false);
-  const [text,setText] = useState('yes');
-  const [name, setName] = useState('');
-  const [des, setDesc] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const [linkText,setLinKText] = useState('');
-  const [linkedArray,setLinkedArray] = useState([])
-  const [digest,setDigest] = useState('')
-  const [isLoading,setIsLoading] = useState(false)
+  const [text, setText] = useState("");
+  const [name, setName] = useState("");
+  const [des, setDesc] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [linkText, setLinKText] = useState("");
+  const [linkedArray, setLinkedArray] = useState([]);
+  const [digest, setDigest] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const onChange = (checked) => {
     if (checked) {
@@ -34,79 +34,72 @@ const NewCampaign = () => {
     }
   };
 
-  const handleLinkedText = (event) => { 
-    if (event.key === 'Enter') {
-    event.preventDefault();
-    setLinkedArray(prevArray => [...prevArray, linkText]);
-    setLinKText('');
-  }
+  const handleLinkedText = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      setLinkedArray((prevArray) => [...prevArray, linkText]);
+      setLinKText("");
+    }
+  };
 
- }
+  const deleteItem = (value) => {
+    const result = linkedArray.filter((item) => item !== value);
+    setLinkedArray([...result]);
+  };
 
-  const deleteItem = (value) => { 
-   const result = linkedArray.filter(item => item !== value)
-   setLinkedArray([...result])
-  }
-  
   const convertDateToISO = (dateString) => {
     if (dateString) {
-        const date = new Date(dateString);
-        return date.toISOString();
-      }
-      return '';
+      const date = new Date(dateString);
+      return date.toISOString();
+    }
+    return "";
   };
 
   const data = {
-    "campaignName": name,
-    "campaignDescription": des,
-    "startDate": convertDateToISO(startDate),
-    "endDate": convertDateToISO(endDate),
-    "digestCampaign":checked ? true : false ,
-    "linkedKeywords":linkedArray,
-    "dailyDigest": digest
-  }
+    campaignName: name,
+    campaignDescription: des,
+    startDate: convertDateToISO(startDate),
+    endDate: convertDateToISO(endDate),
+    digestCampaign: checked ? true : false,
+    linkedKeywords: linkedArray,
+    dailyDigest: digest,
+  };
 
-  const handleSubmit = async() => { 
-    setIsLoading(true)
-    try{ 
-    const  res = await campaignService.postCampaign(data);
-    if(res){ 
-     setIsLoading(false)
-     setModalOpen(true)
-     setText('Camapaign successfully created!')
-    //  toast.success('campaign successfully created')
-    //  console.log('res is here',res)
-     setTimeout(() => { 
-        navigate('/campaign')
-     },1500)
-     setName('');
-     setDesc('')
-     setStartDate('')
-     setEndDate('')
-    setLinKText('')
-    setLinkedArray([])
-    setDigest('')    
+  const handleSubmit = async () => {
+    setIsLoading(true);
+    try {
+      const res = await campaignService.postCampaign(data);
+      if (res) {
+        setIsLoading(false);
+        setModalOpen(true);
+        setText("Camapaign successfully created!");
+        setName("");
+        setDesc("");
+        setStartDate("");
+        setEndDate("");
+        setLinKText("");
+        setLinkedArray([]);
+        setDigest("");
+      }
+    } catch (err) {
+      setIsLoading(false);
+      // console.log("err is here", err);
     }
-    }catch(err){ 
-        setIsLoading(false)
-        console.log('err is here',err)
-    }
-  }
+  };
 
-  const handleCancel = () => { 
-    navigate('/')
-  }
-
-
+  const handleCancel = () => {
+    navigate("/");
+  };
 
   return (
     <Container className="h-[100%] w-[100%] flex flex-col">
-    {modalOpen && text &&
-    <SuccessMessage 
-     setModalOpen = {setModalOpen}
-     text={text}
-     modalOpen = {modalOpen}
-    />}
+      {modalOpen && text && (
+        <SuccessMessage
+          text={text}
+          modalOpen={modalOpen}
+          onClose = {() => setModalOpen(false)}
+        />
+      )}
       <Top className="mt-6">
         <h3 className="text-xl mb-0 leading-7 font-bold font-[work-sans] text-primary">
           Create New Campaign
@@ -186,24 +179,30 @@ const NewCampaign = () => {
                   className="border border-[#999999] rounded-md p-[10px] w-full bg-transparent text-[#999999] font-[nunito] font-medium text-sm leading-5"
                 ></textarea> */}
                 <div className="border border-[#999999] rounded-md p-[10px] w-full bg-transparent">
-                <input 
-                  value={linkText}
-                   placeholder="To add keywords, type your keyword and press enter"
+                  <input
+                    value={linkText}
+                    placeholder="To add keywords, type your keyword and press enter"
                     className="w-full pb-4 outline-none border-none"
-                    onChange={e => setLinKText(e.target.value)}
+                    onChange={(e) => setLinKText(e.target.value)}
                     onKeyDown={handleLinkedText}
-                />
-                <div className="flex gap-1 items-center flex-wrap"> 
-                   { 
-                    linkedArray.length >= 1 && linkedArray.map((item,i) => ( 
-                    <div className="w-[100px] break-words py-[5px] px-[10px] flex items-center justify-between rounded-md bg-primary text-white cursor-pointer" key={i}>
-                    <span className="font-[nunito] font-medium text-[10px] leading-[14px]">{item}</span>
-                    <MdOutlineClose className="text-white" onClick={ () => deleteItem(item)}/>
+                  />
+                  <div className="flex gap-1 items-center flex-wrap">
+                    {linkedArray.length >= 1 &&
+                      linkedArray.map((item, i) => (
+                        <div
+                          className="w-[100px] break-words py-[5px] px-[10px] flex items-center justify-between rounded-md bg-primary text-white cursor-pointer"
+                          key={i}
+                        >
+                          <span className="font-[nunito] font-medium text-[10px] leading-[14px]">
+                            {item}
+                          </span>
+                          <MdOutlineClose
+                            className="text-white"
+                            onClick={() => deleteItem(item)}
+                          />
+                        </div>
+                      ))}
                   </div>
-                    ))
-                   }
-                </div>
-
                 </div>
               </div>
               <div className="flex flex-col space-y-1">
@@ -211,9 +210,10 @@ const NewCampaign = () => {
                   Kindly select how often you want to receive daily digest
                 </label>
                 <select
-                value={digest}
-                onChange={e => setDigest(e.target.value)}
-                 className="border border-[#999999] rounded-md p-[10px] w-[155px] bg-transparent text-[#999999] font-[nunito] font-medium text-sm leading-5">
+                  value={digest}
+                  onChange={(e) => setDigest(e.target.value)}
+                  className="border border-[#999999] rounded-md p-[10px] w-[155px] bg-transparent text-[#999999] font-[nunito] font-medium text-sm leading-5"
+                >
                   <option selected disabled>
                     Select
                   </option>
@@ -226,14 +226,16 @@ const NewCampaign = () => {
           )}
           <div className="pt-4 pb-4 flex items-center gap-4">
             <span
-            onClick={handleCancel}
-             className="border border-primary bg-white text-primary rounded-md w-[196px] pb-2 pt-[10px] flex items-center justify-center cursor-pointer font-[nunito] font-semibold text-sm leading-5">
+              onClick={handleCancel}
+              className="border border-primary bg-white text-primary rounded-md w-[196px] pb-2 pt-[10px] flex items-center justify-center cursor-pointer font-[nunito] font-semibold text-sm leading-5"
+            >
               Cancel
             </span>
-            <span 
-            onClick={handleSubmit}
-            className="bg-primary text-white rounded-md w-[196px] pb-2 pt-[10px] flex items-center justify-center cursor-pointer font-[nunito] font-semibold text-sm leading-5 shadow-emerald-50">
-              {isLoading ? 'loading ...' : 'Create Campaign'}
+            <span
+              onClick={handleSubmit}
+              className="bg-primary text-white rounded-md w-[196px] pb-2 pt-[10px] flex items-center justify-center cursor-pointer font-[nunito] font-semibold text-sm leading-5 shadow-emerald-50"
+            >
+              {isLoading ? "loading ..." : "Create Campaign"}
             </span>
           </div>
         </section>
